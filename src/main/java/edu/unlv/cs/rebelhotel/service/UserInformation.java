46831505f74ba93197d20d8fa286e268718a08c4
unknown <cs473@.(none)>
2011-04-20 19:51:57 -0700

    Change Password functionality added

--- src/main/java/edu/unlv/cs/rebelhotel/service/UserInformation.java
+++ src/main/java/edu/unlv/cs/rebelhotel/service/UserInformation.java
@@ -1,10 +1,20 @@
 package edu.unlv.cs.rebelhotel.service;
 
 import edu.unlv.cs.rebelhotel.domain.Student;
+import edu.unlv.cs.rebelhotel.domain.UserAccount;
 
 public class UserInformation {
 	private Student student;
+	private UserAccount userAccount;
 	
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
 	public Student getStudent() {
 		return student;
 	}
