1a1568bff86cb1a22c9a1d9aa67a8e33255a34e5
unknown <cs473@.(none)>
2011-04-25 21:13:52 -0700

    Change Password complete

--- src/main/java/edu/unlv/cs/rebelhotel/form/FormChangePassword.java
+++ src/main/java/edu/unlv/cs/rebelhotel/form/FormChangePassword.java
@@ -1,34 +1,42 @@
 package edu.unlv.cs.rebelhotel.form;
 
-// A form backing class for use in the work requirement creation form (which lists work templates)
 public class FormChangePassword {
-	
+	private Long id;
+	private Integer version;
 	private String currentPassword;
 	private String newPassword;
 	private String confirmPassword;
 	
-	public String getNewPassword(){
-		return newPassword;
-	}
-	
-	public String getConfirmPassword(){
-		return confirmPassword;
-	}
-	
-
 	public String getCurrentPassword() {
 		return currentPassword;
 	}
-
 	public void setCurrentPassword(String currentPassword) {
 		this.currentPassword = currentPassword;
 	}
-
+	public String getNewPassword() {
+		return newPassword;
+	}
 	public void setNewPassword(String newPassword) {
 		this.newPassword = newPassword;
 	}
-
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	
+
 }
--- src/main/java/edu/unlv/cs/rebelhotel/service/UserInformation.java
+++ src/main/java/edu/unlv/cs/rebelhotel/service/UserInformation.java
@@ -1,9 +1,12 @@
 package edu.unlv.cs.rebelhotel.service;
 
+import java.io.Serializable;
+
 import edu.unlv.cs.rebelhotel.domain.Student;
 import edu.unlv.cs.rebelhotel.domain.UserAccount;
 
-public class UserInformation {
+public class UserInformation implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private Student student;
 	private UserAccount userAccount;
 	
