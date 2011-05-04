a300e4a1711a2f10a6e6dd809224b9f0fb848d97
doccz <d4penguin@gmail.com>
2011-05-04 16:01:39 -0700

    final changes until after presentation

--- src/main/java/edu/unlv/cs/rebelhotel/web/UserAccountController.java
+++ src/main/java/edu/unlv/cs/rebelhotel/web/UserAccountController.java
@@ -1,5 +1,8 @@
 package edu.unlv.cs.rebelhotel.web;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Collection;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -14,13 +17,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriUtils;
+import org.springframework.web.util.WebUtils;
 
 
-@RooWebScaffold(path = "useraccounts", formBackingObject = UserAccount.class)
 @RequestMapping("/useraccounts")
 @Controller
 public class UserAccountController {
@@ -92,4 +97,63 @@ public class UserAccountController {
         return "redirect:/useraccounts?page=" + ((page == null) ? "1" : page.toString()) + "&size=" + ((size == null) ? "10" : size.toString());
     }
 
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("useraccount", UserAccount.findUserAccount(id));
+        model.addAttribute("itemId", id);
+        return "useraccounts/show";
+    }
+
+	@RequestMapping(method = RequestMethod.GET)
+    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model model) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            model.addAttribute("useraccounts", UserAccount.findUserAccountEntries(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo));
+            float nrOfPages = (float) UserAccount.countUserAccounts() / sizeNo;
+            model.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            model.addAttribute("useraccounts", UserAccount.findAllUserAccounts());
+        }
+        return "useraccounts/list";
+    }
+
+	@RequestMapping(params = { "find=ByUserId", "form" }, method = RequestMethod.GET)
+    public String findUserAccountsByUserIdForm(Model model) {
+        return "useraccounts/findUserAccountsByUserId";
+    }
+
+	@RequestMapping(params = "find=ByUserId", method = RequestMethod.GET)
+    public String findUserAccountsByUserId(@RequestParam("userId") String userId, Model model) {
+        model.addAttribute("useraccounts", UserAccount.findUserAccountsByUserId(userId).getResultList());
+        return "useraccounts/list";
+    }
+
+	@RequestMapping(params = { "find=ByUserIdEquals", "form" }, method = RequestMethod.GET)
+    public String findUserAccountsByUserIdEqualsForm(Model model) {
+        return "useraccounts/findUserAccountsByUserIdEquals";
+    }
+
+	@RequestMapping(params = "find=ByUserIdEquals", method = RequestMethod.GET)
+    public String findUserAccountsByUserIdEquals(@RequestParam("userId") String userId, Model model) {
+        model.addAttribute("useraccounts", UserAccount.findUserAccountsByUserIdEquals(userId).getResultList());
+        return "useraccounts/list";
+    }
+
+	@ModelAttribute("usergroups")
+    public Collection<UserGroup> populateUserGroups() {
+        return Arrays.asList(UserGroup.class.getEnumConstants());
+    }
+
+	String encodeUrlPathSegment(String pathSegment, HttpServletRequest request) {
+        String enc = request.getCharacterEncoding();
+        if (enc == null) {
+            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+        }
+        try {
+            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+        }
+        catch (UnsupportedEncodingException uee) {}
+        return pathSegment;
+    }
 }
